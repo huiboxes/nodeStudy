c8d1b67b52bce99205194e467c50572d643d7851
huiboxes <huiboxes@gmail.com>
2022-06-16 23:35:30 +0800

    like-express

--- express-test/app.js
+++ express-test/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next)=>{
-  console.log('请求开始...', req.method, req.url)
+  console.log('请 求开始...', req.method, req.url)
   next()
 })
 
